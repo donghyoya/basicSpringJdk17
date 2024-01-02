1222405bd9ac559112fa3d452ead4bc3098e39d3
donghyoya <dongwooya123@gmail.com>
2024-01-02 21:43:29 +0900

    setting download path api

--- src/main/java/com/java/basic/setup/domain/file/api/FileApi.java
+++ src/main/java/com/java/basic/setup/domain/file/api/FileApi.java
@@ -3,15 +3,20 @@ package com.java.basic.setup.domain.file.api;
 import com.java.basic.setup.domain.file.dto.FileVo;
 import com.java.basic.setup.domain.file.entity.FileMetadata;
 import com.java.basic.setup.domain.file.service.FileService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -29,12 +34,36 @@ public class FileApi {
     }
 
     // FileController 클래스 내에 추가 메서드
-    @PostMapping("/download")
-    public ResponseEntity<FileMetadata> downloadFile(@RequestParam("url") String url) throws IOException {
+    @PostMapping("/download/url")
+    public ResponseEntity<FileMetadata> downloadFilebyUrl(@RequestParam("url") String url)
+            throws IOException {
         FileMetadata fileMetadata = fileService.downloadFileFromUrl(url);
         return ResponseEntity.ok(fileMetadata);
     }
 
+    @GetMapping("/download/path")
+    public ResponseEntity<Resource> downloadFileByPath(@RequestParam("path") String path,
+                                                       HttpServletRequest request,
+                                                       HttpHeaders httpHeaders)
+            throws IOException {
+
+        Resource resource = fileService.downloadFilePath(path);
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            // logger.info("Could not determine file type.");
+        }
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
     @GetMapping("/between")
     public ResponseEntity<List<FileMetadata>> getFilesBetweenDates(
             @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
--- src/main/java/com/java/basic/setup/domain/file/service/FileService.java
+++ src/main/java/com/java/basic/setup/domain/file/service/FileService.java
@@ -4,12 +4,19 @@ import com.java.basic.setup.domain.file.dto.FileVo;
 import com.java.basic.setup.domain.file.entity.FileMetadata;
 import com.java.basic.setup.domain.file.repository.FileRepository;
 import com.java.basic.setup.domain.file.repository.FileRepositorySupport;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,15 +26,18 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class FileService {
 
-    @Autowired
-    private FileRepository fileRepository;
+    Logger logger = LoggerFactory.getLogger(FileService.class);
 
-    @Autowired
-    private FileRepositorySupport fileSupport;
+    private final FileRepository fileRepository;
+    private final FileRepositorySupport fileSupport;
 
     public FileMetadata storeFile(FileVo file) throws IOException {
+
+        logger.info("file name: "+file.getFiles().getName());
+
         MultipartFile multipartFile = file.getFiles();
         String fileName = multipartFile.getOriginalFilename();
         long fileSize = multipartFile.getSize();
@@ -37,6 +47,8 @@ public class FileService {
         // 예를 들어, 파일을 "/uploads" 디렉토리에 저장하고자 한다면
         String uploadDir = "C:\\uploads"; // 실제 사용할 경로로 변경 필요
         Path uploadPath = Paths.get(uploadDir);
+
+        logger.info("file upload path: "+ uploadPath.toString());
 
         //디렉터리 없으면 생성
         if (!Files.exists(uploadPath)) {
@@ -78,6 +90,21 @@ public class FileService {
 
         return fileRepository.save(fileMetadata);
     }
+
+    public Resource downloadFilePath(String filePathString) throws MalformedURLException, FileNotFoundException {
+//        Path filePath = Paths.get("C:\\uploads").resolve(fileName).normalize();
+        Path filePath = Paths.get(filePathString).normalize();
+        Resource resource = new UrlResource(filePath.toUri());
+
+        String fileName = filePath.getFileName().toString();
+
+        if(resource.exists()) {
+            return resource;
+        } else {
+            throw new FileNotFoundException("File not found " + fileName);
+        }
+    }
+
 
     public List<FileMetadata> getFilesBetweenDates(LocalDateTime startDate, LocalDateTime endDate) {
         return fileSupport.findFilesBetweenDates(startDate, endDate);
